d60796646b443fce5617a24dc1472c27361a6575
zk2840174 <zk2840174@gmail.com>
2023-07-05 16:56:48 +0900

    0705

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {RouterProvider} from "react-router-dom";
+import router from './routers/root';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <RouterProvider  router={router}>
     <App />
-  </React.StrictMode>
+  </RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
